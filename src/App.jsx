28ea04f62e3677292cef11b5432414d5b2ccea3e
TJCole03 <tjc6254@gmail.com>
2023-11-16 06:15:06 -0500

    First Commit, rendered App.jsx page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+    state = {
+        advice: ' '
+    }
+
+    render() {
+        return ( 
+                <h1>APP</h1>
+            )
+    }
+}
+
+export default App;
